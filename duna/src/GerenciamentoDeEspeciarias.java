a731028e4a37e357500be66366added9bfedee77
Iuri <133531559+yIuriy@users.noreply.github.com>
2025-04-27 08:16:10 -0300

    Update GerenciamentoDeEspeciarias.java

--- duna/src/GerenciamentoDeEspeciarias.java
+++ duna/src/GerenciamentoDeEspeciarias.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class GerenciamentoDeEspeciarias {
-
+    
     public static void main(String[] args) throws InterruptedException {
         int colheitadeirasDisponiveis;
         int quantidadeMelange;
